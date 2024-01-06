c93b7011963771dd6a39eed76c11297135df19fe
rifqilutfanpadl <114246084+RIFQILUTFANPADL@users.noreply.github.com>
2024-01-06 14:01:01 +0700

    Update Menu.js
    File Menu.js berisi komponen React yang menampilkan halaman menu dari aplikasi restoran. Komponen ini mencakup elemen-elemen seperti judul, daftar kategori menu, dan dapat diubah atau diperluas sesuai kebutuhan.

---
+++ src/pages/Menu.js
@@ -0,0 +1,19 @@
+// src/pages/Menu.js
+
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>
+      <h1>Our Menu</h1>
+      <ul>
+        <li>Appetizers</li>
+        <li>Main Courses</li>
+        <li>Desserts</li>
+      </ul>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default Menu;
