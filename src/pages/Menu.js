23865257b37fe344c8d8d2f095695e5cee060bb1
rifqilutfanpadl <114246084+RIFQILUTFANPADL@users.noreply.github.com>
2024-01-06 21:33:28 +0700

    Update Menu.js
    File Menu.js berisi komponen React untuk halaman menu dari aplikasi restoran. Halaman ini memperlihatkan kepada pengunjung beragam menu lezat yang tersedia di restoran, disiapkan dengan bahan-bahan segar dan berkualitas tinggi.

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,35 +1,16 @@
-// src/pages/Menu.js
+/* src/pages/Menu.js */
 
-import React, { useState, useEffect } from 'react';
-import { getMenuData } from '../services/ApiService';
-import Header from '../components/Header';
+import React from 'react';
 
 const Menu = () => {
-  const [menuData, setMenuData] = useState([]);
-
-  useEffect(() => {
-    // Panggil fungsi getMenuData saat komponen di-mount
-    const fetchData = async () => {
-      try {
-        const data = await getMenuData();
-        setMenuData(data);
-      } catch (error) {
-        console.error('Error fetching menu data:', error);
-      }
-    };
-
-    fetchData();
-  }, []); // Dependensi kosong, artinya efek hanya dijalankan sekali saat komponen di-mount
-
   return (
     <div>
-      <Header />
-      <h2>Menu Ubicom Restaurant</h2>
-      <ul>
-        {menuData.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      <h2>Our Menu</h2>
+      <p>
+        Explore our diverse menu featuring a variety of delicious dishes,
+        prepared with fresh and high-quality ingredients.
+      </p>
+      {/* Add more content as needed */}
     </div>
   );
 };
