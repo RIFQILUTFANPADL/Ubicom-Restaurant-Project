6bf3c40b0c78755991143a9327ce534244a18f45
rifqilutfanpadl <114246084+RIFQILUTFANPADL@users.noreply.github.com>
2024-01-06 15:14:42 +0700

    Update helpers.js
    File helpers.js berisi dua fungsi, yaitu calculateTotalPrice untuk menghitung total harga dari keranjang belanja dan formatCurrency untuk memformat harga ke dalam format mata uang Indonesia (IDR). Kedua fungsi ini berguna dalam menyediakan informasi terkait harga dalam aplikasi

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,14 +1,13 @@
 // src/utils/helpers.js
 
-// Contoh fungsi utilitas
-export function formatCurrency(amount) {
-  // Implementasi format mata uang sesuai kebutuhan
-  const formattedAmount = new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: 'IDR',
-  }).format(amount);
+// Fungsi untuk menghitung total harga dari keranjang belanja
+const calculateTotalPrice = (cartItems) => {
+  return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+};
 
-  return formattedAmount;
-}
+// Fungsi untuk memformat harga ke dalam format mata uang
+const formatCurrency = (value) => {
+  return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(value);
+};
 
-// Add more utility functions as needed
+export { calculateTotalPrice, formatCurrency };
