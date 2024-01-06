a048a5c3d931d33555b9ad334c7143062f7a3dc1
rifqilutfanpadl <114246084+RIFQILUTFANPADL@users.noreply.github.com>
2024-01-06 22:01:24 +0700

    Update Home.js
    File Home.js: Menampilkan halaman utama restoran dengan menyertakan komponen Header dan konten lain yang bisa ditambahkan sesuai progres proyek.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,16 @@
 /* src/pages/Home.js */
 
-import React, { useState, useEffect } from 'react';
-import ApiService from '../services/ApiService';
+import React from 'react';
+import Header from '../components/Header';
 
-function Home() {
-  const [daftarMenu, setDaftarMenu] = useState([]);
-
-  useEffect(() => {
-    // Ambil daftar menu saat komponen dipasang
-    const ambilDaftarMenu = async () => {
-      try {
-        const data = await ApiService.getDaftarMenu();
-        setDaftarMenu(data);
-      } catch (error) {
-        console.error('Error saat mengambil daftar menu:', error);
-      }
-    };
-
-    ambilDaftarMenu();
-  }, []);
-
+const Home = () => {
   return (
     <div>
+      <Header />
       <h1>Selamat Datang di Restoran Kami!</h1>
-      <p>Jelajahi menu lezat kami:</p>
-      <ul>
-        {daftarMenu.map((item) => (
-          <li key={item.id}>{item.nama} - Rp. {item.harga}</li>
-        ))}
-      </ul>
+      {/* Tambahkan konten lain sesuai progres proyek */}
     </div>
   );
-}
+};
 
 export default Home;
